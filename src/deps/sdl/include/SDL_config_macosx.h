a5666c14667ef884a431856cda60c1079b9e82ba
Valerio Santinelli <santinelli@altralogica.it>
2018-09-25 11:37:57 +0200

    Disabled Vulkan and Metal in macOS's SDL config

--- src/deps/sdl/include/SDL_config_macosx.h
+++ src/deps/sdl/include/SDL_config_macosx.h
@@ -182,13 +182,13 @@
 #endif
 
 #ifndef SDL_VIDEO_RENDER_OGL_ES2
-#define SDL_VIDEO_RENDER_OGL_ES2 1
+#define SDL_VIDEO_RENDER_OGL_ES2 0
 #endif
 
 #ifndef SDL_VIDEO_RENDER_METAL
 /* Metal only supported on 64-bit architectures with 10.11+ */
 #if TARGET_CPU_X86_64 && (MAC_OS_X_VERSION_MAX_ALLOWED >= 101100)
-#define SDL_VIDEO_RENDER_METAL    1
+#define SDL_VIDEO_RENDER_METAL    0
 #else
 #define SDL_VIDEO_RENDER_METAL    0
 #endif
@@ -199,10 +199,10 @@
 #define SDL_VIDEO_OPENGL    1
 #endif
 #ifndef SDL_VIDEO_OPENGL_ES2
-#define SDL_VIDEO_OPENGL_ES2    1
+#define SDL_VIDEO_OPENGL_ES2    0
 #endif
 #ifndef SDL_VIDEO_OPENGL_EGL
-#define SDL_VIDEO_OPENGL_EGL    1
+#define SDL_VIDEO_OPENGL_EGL    0
 #endif
 #ifndef SDL_VIDEO_OPENGL_CGL
 #define SDL_VIDEO_OPENGL_CGL    1
@@ -214,7 +214,7 @@
 /* Enable Vulkan support */
 /* Metal/MoltenVK/Vulkan only supported on 64-bit architectures with 10.11+ */
 #if TARGET_CPU_X86_64 && (MAC_OS_X_VERSION_MAX_ALLOWED >= 101100)
-#define SDL_VIDEO_VULKAN 1
+#define SDL_VIDEO_VULKAN 0
 #else
 #define SDL_VIDEO_VULKAN 0
 #endif
