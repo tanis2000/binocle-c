0fec8f123769caea64a020e6e1638e841665aaf2
Valerio Santinelli <santinelli@gmail.com>
2018-11-28 04:07:18 +0100

    Fixes for SDL on Windows

--- build/cmake/BinocleWindows.cmake
+++ build/cmake/BinocleWindows.cmake
@@ -5,5 +5,5 @@ add_definitions (-DNOMINMAX) # min and max definitions are clashing with glm
 #add_definitions (-DNOGDI) # the problem is that excluding wingdi also excludes some pieces needed by SDL
 add_definitions(-DGLEW_STATIC) # This has to be defined here or the program will get the header with the shared declspec
 
-set(BINOCLE_LINK_LIBRARIES ${BINOCLE_LINK_LIBRARIES} glew winmm.lib Opengl32.lib Ws2_32.lib Mincore.lib imm32.lib version.lib)
+set(BINOCLE_LINK_LIBRARIES ${BINOCLE_LINK_LIBRARIES} glew winmm.lib Opengl32.lib Ws2_32.lib Mincore.lib imm32.lib version.lib hid.lib setupapi.lib)
 message("Windows list of libraries we will link against: ${BINOCLE_LINK_LIBRARIES}")
