c21743683ce164b92ef28e004c6530c2b9fedb2b
Valerio Santinelli <santinelli@altralogica.it>
2018-11-29 19:29:59 +0100

    binocle_subtexture had wrong definitions in the header

--- src/binocle/core/binocle_subtexture.h
+++ src/binocle/core/binocle_subtexture.h
@@ -20,9 +20,9 @@ binocle_subtexture binocle_subtexture_with_texture(struct binocle_texture *textu
 binocle_subtexture binocle_subtexture_with_subtexture(binocle_subtexture *subtexture, float x, float y, float width, float height);
 uint64_t binocle_subtexture_get_x(binocle_subtexture subtexture);
 uint64_t binocle_subtexture_get_y(binocle_subtexture subtexture);
-uint64_t binocle_subtexture_get_width(binocle_subtexture subtexture);
-uint64_t binocle_subtexture_get_height(binocle_subtexture subtexture);
-kmVec2 binocle_subtexture_get_size(binocle_subtexture subtexture);
+uint64_t binocle_subtexture_get_width(binocle_subtexture *subtexture);
+uint64_t binocle_subtexture_get_height(binocle_subtexture *subtexture);
+kmVec2 binocle_subtexture_get_size(binocle_subtexture *subtexture);
 kmVec2 binocle_subtexture_get_center(binocle_subtexture *subtexture);
 kmAABB2 binocle_subtexture_get_frame(binocle_subtexture subtexture, uint64_t index, uint64_t frame_width, uint64_t frame_height);
 kmAABB2 binocle_subtexture_get_absolute_clip_rect(binocle_subtexture subtexture, kmAABB2 relative_clip_rect);
