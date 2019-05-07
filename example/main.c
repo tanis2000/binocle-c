a4b1e28a23c5e4b5e89fc55f2dd05c35187bffa5
Valerio Santinelli <santinelli@altralogica.it>
2019-05-07 21:21:19 +0200

    Example working with the latest version of Binocle

--- example/main.c
+++ example/main.c
@@ -27,8 +27,8 @@
 #include "sys_config.h"
 
 //#define GAMELOOP 1
-#define DEMOLOOP
-//#define TWODLOOP
+//#define DEMOLOOP
+#define TWODLOOP
 
 binocle_window window;
 binocle_input input;
@@ -37,7 +37,7 @@ binocle_camera camera;
 binocle_sprite player;
 kmVec2 player_pos;
 binocle_gd gd;
-binocle_bitmapfont font;
+binocle_bitmapfont *font;
 binocle_image font_image;
 binocle_texture font_texture;
 binocle_material font_material;
@@ -77,10 +77,15 @@ void main_loop() {
   }
 
   binocle_window_clear(&window);
-  binocle_sprite_draw(player, &gd, (uint64_t)player_pos.x, (uint64_t)player_pos.y, adapter.viewport);
+  kmVec2 scale;
+  scale.x = 1.0f;
+  scale.y = 1.0f;
+  binocle_sprite_draw(player, &gd, (uint64_t)player_pos.x, (uint64_t)player_pos.y, adapter.viewport, 0, scale, &camera);
   char fps_str[256];
   sprintf(fps_str, "FPS: %d", binocle_window_get_fps(&window));
-  binocle_bitmapfont_draw_string(font, fps_str, 32, &gd, 0, window.height - 32, adapter.viewport);
+  kmMat4 view_matrix;
+  kmMat4Identity(&view_matrix);
+  binocle_bitmapfont_draw_string(font, fps_str, 32, &gd, 0, window.height - 32, adapter.viewport, binocle_color_white(), view_matrix);
   //binocle_sprite_draw(font_sprite, &gd, (uint64_t)font_sprite_pos.x, (uint64_t)font_sprite_pos.y, adapter.viewport);
   binocle_window_refresh(&window);
   binocle_window_end_frame(&window);
@@ -237,7 +242,9 @@ void main_loop() {
   time += (binocle_window_get_frame_time(&window) / 1000.0);
   char fps_str[256];
   sprintf(fps_str, "FPS: %d", binocle_window_get_fps(&window));
-  binocle_bitmapfont_draw_string(font, fps_str, 32, &gd, 0, window.height - 32, adapter.viewport, binocle_color_white());
+  kmMat4 view_matrix;
+  kmMat4Identity((&view_matrix));
+  binocle_bitmapfont_draw_string(font, fps_str, 32, &gd, 0, window.height - 32, adapter.viewport, binocle_color_white(), view_matrix);
 
   binocle_window_refresh(&window);
   binocle_window_end_frame(&window);
@@ -304,7 +311,7 @@ int main(int argc, char *argv[])
   font_material = binocle_material_new();
   font_material.texture = &font_texture;
   font_material.shader = &shader;
-  font.material = &font_material;
+  font->material = &font_material;
   font_sprite = binocle_sprite_from_material(&font_material);
   font_sprite_pos.x = 0;
   font_sprite_pos.y = -256;
