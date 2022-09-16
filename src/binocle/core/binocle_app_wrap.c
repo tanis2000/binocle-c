75a4528670f8be74c500a99d1dfad3cbbf3439f0
Valerio Santinelli <santinelli@altralogica.it>
2022-09-16 17:38:30 +0200

    Added the Lua wrapper for binocle_app

---
+++ src/binocle/core/binocle_app_wrap.c
@@ -0,0 +1,23 @@
+//
+// Created by Valerio Santinelli on 02/02/2020.
+//
+
+#include "binocle_lua.h"
+#include "binocle_app.h"
+
+int l_binocle_app_assets_dir(lua_State *L) {
+  char *assets_dir = "/assets/";
+  lua_pushstring(L, assets_dir);
+  return 1;
+}
+
+static const struct luaL_Reg app [] = {
+  {"assets_dir", l_binocle_app_assets_dir},
+  {NULL, NULL}
+};
+
+int luaopen_app(lua_State *L) {
+  luaL_newlib(L, app);
+  lua_setglobal(L, "app");
+  return 1;
+}
---
+++ src/binocle/core/binocle_app_wrap.h
@@ -0,0 +1,10 @@
+//
+// Created by Valerio Santinelli on 02/02/2020.
+//
+
+#ifndef LUA_BINOCLE_APP_WRAP_H
+#define LUA_BINOCLE_APP_WRAP_H
+
+int luaopen_app(lua_State *L);
+
+#endif //LUA_BINOCLE_APP_WRAP_H
